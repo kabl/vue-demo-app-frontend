435be415689e6cf46472d1ad210b81e85e619013
kabl <kasimir@digitalizeit.io>
2020-09-20 01:22:31 +0200

    adding vuex

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,25 +1,30 @@
 <template>
-  <v-card>
-    <v-card-title>User Properties</v-card-title>
-    <v-card-text>
-      <v-simple-table>
-        <template v-slot:default>
-          <thead>
-            <tr>
-              <th class="text-left">Name</th>
-              <th class="text-left">Property</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in desserts" :key="item.name">
-              <td>{{ item.name }}</td>
-              <td>{{ item.calories }}</td>
-            </tr>
-          </tbody>
-        </template>
-      </v-simple-table>
-    </v-card-text>
-  </v-card>
+  <div>
+    <div>{{ count }}
+      <button v-on:click="$store.commit('increment')">Add 1</button>
+    </div>
+    <v-card>
+      <v-card-title>User Properties</v-card-title>
+      <v-card-text>
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Name</th>
+                <th class="text-left">Property</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in desserts" :key="item.name">
+                <td>{{ item.name }}</td>
+                <td>{{ item.calories }}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -43,6 +48,12 @@ export default {
         }
       ]
     };
-  }
+  },
+  computed: {
+    count() {
+      return this.$store.state.count;
+    }
+  },
+
 };
 </script>
